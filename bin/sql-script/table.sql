b0ab019dc01327eb7f06dc0ebb1c8ed1c2ba6d84
LSH\lsh <dlrnld@jongdali.com>
2018-02-02 18:40:15 +0900

    OAuth2 구축완료

---
+++ bin/sql-script/table.sql
@@ -0,0 +1,70 @@
+CREATE SCHEMA `handcoding` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `code` (
+	`code`       CHAR(20)      NOT NULL COMMENT '코드값',
+	`parentCode` CHAR(20)      NULL     DEFAULT '' COMMENT '상위 코드값',
+	`descript`   VARCHAR(150)  NOT NULL COMMENT '코드에 대한 설명'
+);
+
+ALTER TABLE `code`
+	ADD CONSTRAINT
+		PRIMARY KEY (
+			`code`,
+            `parentCode`
+		);
+
+CREATE TABLE `user` (
+	`id`                 VARCHAR(20)	NOT NULL COMMENT 'ID',
+	`pw`       		 	 VARCHAR(100)	NOT NULL COMMENT '암호',
+	`name`               VARCHAR(20)	NOT NULL COMMENT '이름',
+	`email`              VARCHAR(256)	NOT NULL COMMENT '이메일 주소',
+	`userTypeCode`       CHAR(20)		NOT NULL COMMENT '사용자타입code',    
+	`statusCode`         CHAR(20)		NOT NULL COMMENT '상태code',
+	`regDate`            DATETIME		NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'now()'
+);
+
+ALTER TABLE `user`
+	ADD CONSTRAINT
+		PRIMARY KEY (
+			`id`
+		);
+        
+CREATE TABLE `email_confirm` (
+	`emailKey`		VARCHAR(255) NOT NULL COMMENT '키값',
+	`id`			VARCHAR(20)  NOT NULL COMMENT 'ID',    
+	`userTypeCode`	CHAR(20)      NOT NULL COMMENT '사용자타입code',
+	`regDate`		DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'now()',
+	`expiredDate`	DATETIME     NOT NULL COMMENT '유효하기까지의 날짜'
+);
+
+ALTER TABLE `email_confirm`
+	ADD CONSTRAINT
+		PRIMARY KEY (
+			`emailKey`
+		);
+
+create table oauth_client_details (
+  client_id VARCHAR(256) ,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(256),
+  userId VARCHAR(20)
+);
+
+create table oauth_access_token (
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication BLOB,
+  refresh_token VARCHAR(256)
+);
+
---
+++ src/main/java/com/handcoding/oauth/OAuth2Application.java
@@ -0,0 +1,91 @@
+package com.handcoding.oauth;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.handcoding.oauth.filter.AccessLogFilter;
+
+@EnableAuthorizationServer
+@SpringBootApplication
+public class OAuth2Application extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
+	
+	/**
+	 * AccessLog filter 설정
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean accessLogFilterBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new AccessLogFilter());
+		return registrationBean;
+	}
+	
+}
+
+@Configuration
+class OAuth2Configuration {
+
+    @Bean
+    public TokenStore jdbcTokenStore(DataSource dataSource) {
+        return new JdbcTokenStore(dataSource);
+        // jwt 토큰시
+//        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    @Primary
+    public JdbcClientDetailsService jdbcClientDetailsService(DataSource dataSource) {
+        return new JdbcClientDetailsService(dataSource);
+    }
+    
+    // jwt 토큰시
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        return new JwtAccessTokenConverter();
+//    }
+    
+}
+
+@Configuration
+class JwtOAuth2AuthorizationServerConfiguration extends OAuth2AuthorizationServerConfiguration {
+
+	private final ClientDetailsService clientDetailsService;
+	
+	public JwtOAuth2AuthorizationServerConfiguration(BaseClientDetails details,
+			AuthenticationManager authenticationManager, ObjectProvider<TokenStore> tokenStore,
+			ObjectProvider<AccessTokenConverter> tokenConverter, AuthorizationServerProperties properties, ClientDetailsService clientDetailsService) {
+		super(details, authenticationManager, tokenStore, tokenConverter, properties);
+		this.clientDetailsService = clientDetailsService;
+	}
+
+	/**
+	 * DB에 저장된 사용자 정보로 검증처리 : client_id, client_secret
+	 */
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.withClientDetails(clientDetailsService);
+	}
+    
+}
